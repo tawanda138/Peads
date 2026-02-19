9ca35f99deea2acd5544af38cccd9b7999ab96d5
T <tawandamangawa138@gmail.com>
2026-02-19 15:41:02 +0200

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   define: {
+    // This injection happens during Vercel's build step
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY || '')
   },
   build: {
